cd8618f1f21e89ee2f8be7b0713bae03bd66c22e
Bhunakit <63712938+Bhunakit@users.noreply.github.com>
2020-07-04 13:34:16 +0700

    Update secondary.py

--- secondary.py
+++ secondary.py
@@ -1,14 +1,28 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 import numpy as np
 
 second = Blueprint('second', __name__, static_folder='static', template_folder='templates')
 
-@second.route('/operations', methods=['GET', 'POST'])
-def operations():
+model = LinearRegression()
+
+@second.route('/linreg', methods=['GET', 'POST'])
+def linreg():
 	if request.method == 'POST':
-		result = float(request.form['num1']) * float(request.form['num2']) 
+		x = list(map(int, request.form['x'].split()))
+		y = list(map(int, request.form['y'].split()))
+		x = np.array(x).reshape((-1, 1))
+		model.fit(x, y)
+		flash('Model Trained')
+		return redirect('/linregpredict')
+	return render_template('operations.html')
+
+@second.route('/linregpredict', methods=['GET', 'POST'])
+def linregpredict():
+	if request.method == 'POST':
+		num = float(request.form['num'])
+		result = model.predict([[num]])
 		flash(result)
 		return render_template('operations.html')
 	return render_template('operations.html')
-
